5d254cba95e203d0a12a69983faf50b9e49293e0
Asafergi <asafargi12@gmail.com>
2023-11-20 14:53:58 +0200

    first commit

--- routes/index.js
+++ routes/index.js
@@ -20,40 +20,6 @@ router.get('/', function(req, res) {
 
 
 
-
-
-// router.use(express.static(path.join(__dirname, 'public')));
-/*
-router.get('/calc', function(req, res) {
-  return res.sendFile("calculator.html", {root: path.join('public','calc')});
-});
-
-
-router.get('/weather', function(req, res) {
-  return res.sendFile("countiresIndex.html", {root: path.join('public','weather')});
-});
-
-
-router.get('/users/register', function(req, res) {
-  return res.sendFile("register.html", {root: path.join('public','register')});
-});
-
-router.get('/users/web', function(req, res) {
-  return res.sendFile("web.html", {root: path.join('public','web')});
-});
-router.get('/users/webDrink', function(req, res) {
-  return res.sendFile("webDrink.html", {root: path.join('public','webDrink')});
-});
-router.get('/users/webHamama', function(req, res) {
-  return res.sendFile("webHamama.html", {root: path.join('public','webHamama')});
-});
-router.get('/users/webFruit', function(req, res) {
-  return res.sendFile("webFruit.html", {root: path.join('public','webFruit')});
-});
-
-
-/* My project. */
-
 router.get('/users/project', function(req, res) {
   return res.sendFile("index.html", {root: path.join('public','project')});
 });
